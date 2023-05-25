0201fc86fc89e54d83454aa3e3a5e0ce6d241801
nikiski <inikiski@gmail.com>
2023-05-25 16:03:39 +0800

    delete cron

--- service/data_autopush.go
+++ service/data_autopush.go
@@ -1,17 +1,19 @@
 package service
 
 import (
-	"github.com/robfig/cron/v3"
+	"log"
+	"time"
 )
 
 // 每间隔1min,数据推送给前端
 func AutoPushEveryMin() {
-	crontab := cron.New(cron.WithSeconds())
-	spec := "@every 1m"
-	task := func() {
-		GetDataOneMin()
+	ticker := time.NewTicker(time.Minute)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			GetDataOneMin()
+			log.Println("Auto push")
+		}
 	}
-	crontab.AddFunc(spec, task)
-	crontab.Start()
-	select {}
 }
